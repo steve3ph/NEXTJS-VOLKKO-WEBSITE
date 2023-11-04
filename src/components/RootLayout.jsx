62bd00ce1a37dab9fc7a7c0721d4bed41c393eb9
steve3ph <steve3phtech@gmail.com>
2023-11-04 21:48:45 +0530

    name1

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -27,7 +27,7 @@ const Header = ({
       <div className="flex items-center justify-between">
         {/* Logo */}
         <Link href={"/"} aria-label="Home">
-          <Logo invert={invert}>Volkkommen</Logo>
+          <Logo invert={invert}>Studio_clone</Logo>
         </Link>
         <div className="flex items-center gap-x-8">
           <Button href={"/contact"} invert={invert}>
@@ -168,7 +168,7 @@ const RootLayoutInner = ({ children }) => {
                 <div className="grid grid-cols-1 gap-y-10 pb-16 pt-10 sm:grid-cols-2 sm:pt-16">
                   <div>
                     <h2 className="font-display text-base font-semibold text-white">
-                      Our office
+                      Our offices
                     </h2>
                     <Offices
                       invert
